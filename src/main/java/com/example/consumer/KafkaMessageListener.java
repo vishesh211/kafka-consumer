1c049fb35e2f6ebaf9770d582e2ec5f28172617e
vishesh <visheshtiwari@ggx.io>
2023-09-16 13:33:37 +0530

    created multiple consumer listening to a topic and with a group-id in kafka

---
+++ src/main/java/com/example/consumer/KafkaMessageListener.java
@@ -0,0 +1,37 @@
+package com.example.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaMessageListener {
+
+    Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+    @KafkaListener(topics = "demo-6", groupId = "consumer-2")
+    public void consume1(String message) {
+        log.info("Message consumed by consumer1: " + message);
+    }
+
+    @KafkaListener(topics = "demo-6", groupId = "consumer-2")
+    public void consume2(String message) {
+        log.info("Message consumed by consumer2: " + message);
+    }
+
+    @KafkaListener(topics = "demo-6", groupId = "consumer-2")
+    public void consume3(String message) {
+        log.info("Message consumed by consumer3: " + message);
+    }
+
+    @KafkaListener(topics = "demo-6", groupId = "consumer-2")
+    public void consume4(String message) {
+        log.info("Message consumed by consumer4: " + message);
+    }
+
+    @KafkaListener(topics = "demo-6", groupId = "consumer-2")
+    public void consume5(String message) {
+        log.info("Message consumed by consumer5: " + message);
+    }
+}
